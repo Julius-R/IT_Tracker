48e9b6d565e4da9f773dc453161cc5db9f4656ee
Julius-R <julius.robinson95@yahoo.com>
2021-07-09 08:23:11 -0600

    added uuid and finished header and ticket form

--- src/Components/TicketCreator.jsx
+++ src/Components/TicketCreator.jsx
@@ -1,11 +1,38 @@
 import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
 import { toggleModal } from "../redux/reducers/modalSlice";
+import { addTicket } from "../redux/reducers/ticketSlice";
 import { Modal, Button, Form, Container } from "react-bootstrap";
 
 export default function TicketCreator() {
   const showModal = useSelector((state) => state.modalActive.isActive);
   const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    const form = event.currentTarget;
+    const [category, description, priority] = form.elements;
+    if (description.value.match(/^\s*$/)) {
+      alert("Please do not leave this field empty");
+    } else {
+      createTicket(category.value, description.value, priority.value);
+      dispatch(toggleModal());
+    }
+  };
+
+  const createTicket = (category, description, priority) => {
+    dispatch(
+      addTicket({
+        id: uuidv4(),
+        category,
+        description,
+        priority,
+        dateCreated: Date.now(),
+      })
+    );
+  };
   return (
     <Modal
       size="lg"
@@ -13,22 +40,36 @@ export default function TicketCreator() {
       onHide={() => dispatch(toggleModal())}
       aria-labelledby="example-modal-sizes-title-lg">
       <Container className="ticketCreator">
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text-muted">
-              We'll never share your email with anyone else.
-            </Form.Text>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="Category of issue">
+            <Form.Label>Category</Form.Label>
+            <Form.Control as="select" custom>
+              <option>Software</option>
+              <option>Hardware</option>
+              <option>Unsure</option>
+            </Form.Control>
           </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+          <Form.Group controlId="Description of issue">
+            <Form.Label>Description</Form.Label>
+            <Form.Control
+              required
+              as="textarea"
+              rows={3}
+              placeholder="Give a detailed description of your issue"
+            />
+          </Form.Group>
+          <Form.Group controlId="Priority Selector">
+            <Form.Label>Priority</Form.Label>
+            <Form.Control as="select" custom>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </Form.Control>
           </Form.Group>
           <Button variant="primary" type="submit">
             Submit
           </Button>{" "}
-          <Button variant="danger" type="submit">
+          <Button variant="danger" onClick={() => dispatch(toggleModal())}>
             Cancel
           </Button>
         </Form>
