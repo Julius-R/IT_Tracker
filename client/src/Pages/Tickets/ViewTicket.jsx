835bf7374f5da7725b9dd1cdf957e55d015edacd
Julius Robinson <julius.robinson95@yahoo.com>
2021-07-11 20:39:39 -0600

    Update ViewTicket.jsx

--- client/src/Pages/Tickets/ViewTicket.jsx
+++ client/src/Pages/Tickets/ViewTicket.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import Header from "../../Components/Header.jsx";
 
-export default function ViewTicket() {
+export default function ViewTicket(props) {
+  /*
+  TODO:
+  Create call to backend using the ID of the current ticket
+  Create UI for showing ticket information
+  Add call to backend to submit changes to DB
+  Redirect back to Home routes
+  */
   return (
     <>
       <Header />
+      {console.log(props)}
     </>
   );
 }
