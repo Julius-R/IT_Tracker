0168738bc1512d8ce8898f5c934f362a03e1d510
Julius Robinson <julius.robinson95@yahoo.com>
2021-07-11 20:40:48 -0600

    Update Ticket.jsx

--- client/src/Pages/Home/Ticket.jsx
+++ client/src/Pages/Home/Ticket.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import {Link} from "react-router-dom"
 
 export default function Ticket({ ticket }) {
   return (
     <tr>
-      <td>{ticket.id}</td>
+      <td><Link to={`/ticket/${ticket.id}`}>{ticket.id}</Link></td>
       <td>{ticket.category}</td>
       <td>{ticket.description}</td>
       <td>{ticket.priority}</td>
